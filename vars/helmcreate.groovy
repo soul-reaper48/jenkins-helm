9ac05d4c76f8e03cdd87ea84aa54f962ae9da46f
soul-reaper48 <31500908+soul-reaper48@users.noreply.github.com>
2019-04-11 11:01:53 +0530

    Create helmcreate.groovy

---
+++ vars/helmcreate.groovy
@@ -0,0 +1,29 @@
+def call(String chart-name){
+
+	echo $(chart-name);
+	
+	#create the helm chart
+	sh 'helm create $(chart-name)'
+	
+	#make changes in values.yaml
+	sh '''
+	cat <<EOF > helmcreate.sh
+	#!/bin/bash
+
+	image=soulreaper48/sample-web
+	tag="1.0"
+	version=0.1.0
+	container-port=8000
+
+
+	sed -i "s/nginx/$image/g" ./$(chart-name)/values.yaml
+	sed -i "s/stable/$tag/g" ./$(chart-name)/values.yaml
+	sed -i "s/0.1.0/$version/g" ./$(chart-name)/Chart.yaml
+	sed -i "s/80/$container-port/g" ./$(chart-name)/templates/deployment.yaml
+	'''
+	sh 'chmod +x helmcreate.sh'
+	sh './helmcreate.sh'
+	sh 'rm helmcreate.sh'
+	
+
+}
